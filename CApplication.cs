49f1140beaaf2290b28ca4cbbd7224e38c5ad35d
greatspirit-de <computer@accd.de>
2022-07-28 22:17:19 +0200

    Add files via upload

---
+++ CApplication.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Verwalten
+{
+    public static class CApplication
+    {
+        public static string progname { get; set; }
+        public static int progvers { get; set; }
+        public static string connstr { get; set; }
+        public static string DokuDir { get; set; }
+        public static string VorlagenDir { get; set; }
+        public static string VorlagenKfzDir { get; set; }
+        public static string TempDir { get; set; }
+        public static int dbVersion { get; set; }
+        public static string userName { get; set; }
+        public static DateTime UserLoginTime { get; set; }
+
+        public static int aktJahr { get; set; }
+        public static string FibuPath { get; set; }
+        public static int ErlKto { get; set; }
+
+    }
+}
+
+
+
